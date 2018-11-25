f0cab002c1587d35e06edb15df3e06525ef8fd0f
Jan Jungnickel <jan@jungnickel.com>
2018-11-25 20:14:13 +0100

    Switch from Spacemacs to Doom-Emacs

---
+++ emacs/.doom.d/config.el
@@ -0,0 +1,9 @@
+;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
+
+(setq user-email-address "jan@jungnickel.com"
+      user-full-name "Jan Jungnickel")
+
+(setq doom-theme 'doom-one)
+
+(setq doom-font (font-spec :family "Iosevka Nerd Font" :size 14 :weight 'light))
+(setq doom-big-font (font-spec :family "Iosevka Nerd Font" :size 24 :weight 'light))
