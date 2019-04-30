47574e6b427f69d1b421d2e40ce26d73a9147217
Jan Jungnickel <jan@jungnickel.com>
2019-04-30 13:42:05 +0200

    :wrench: Bump emacs config

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -5,7 +5,7 @@
 
 (setq doom-theme 'doom-one)
 
-(setq doom-font (font-spec :family "Iosevka Nerd Font" :weight 'light :size 14)
+(setq doom-font (font-spec :family "Iosevka Nerd Font" :size 13 :weight 'light)
       doom-variable-pitch-font (font-spec :family "Fira Sans")
       doom-unicode-font (font-spec :family "DejaVu Sans Mono")
       doom-big-font (font-spec :family "Iosevka Nerd Font" :size 24 :weight 'light))
